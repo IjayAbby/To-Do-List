b8c12ff1b85fa8319eebed80d8884038a7964798
LOctavio <luis_ram1997@hotmail.com>
2021-07-29 12:12:20 -0500

    Add test for addTask method

--- src/tests/task.test.js
+++ src/tests/task.test.js
@@ -1,11 +1,19 @@
-const { expect, test } = require('@jest/globals')
-const Task = require ('./task.js')
+const { expect, test } = require('@jest/globals');
+const Task = require ('./task.js');
+const addTask = require('./addTask.js');
 
-
+const myTask = new Task("", false, 0);
+const tasks = [];
+const input = "new task";
 
 describe ('add task', () => {
     test('description is empty string', () => {
-        expect(Task).toEqual([]);
-    }) ;
+        expect(myTask.description).toEqual("");
+    });
+
+    test('add new task to array', () => {
+        addTask(tasks, input);
+        expect(tasks.length).toBe(1);
+    });
 });
 
