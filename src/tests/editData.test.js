5d920a6439f811a5a678ca2745fe22e241db45f0
IjayAbby <inyangasi4@gmail.com>
2021-07-31 00:23:47 +0300

    Update complete status

--- src/tests/editData.test.js
+++ src/tests/editData.test.js
@@ -1,8 +1,9 @@
-const { describe, test } = require('@jest/globals');
+const { describe, test, expect } = require('@jest/globals');
 const editItem = require('./editData');
 const jsdom = require('jsdom');
 const toDoTasks = require('./todo');
 const LocalStorage = require('./localStorage');
+const refrestCompletedTask = require('./completedTask');
 const { JSDOM } = jsdom;
 const dom = new JSDOM('<!DOCTYPE html><ul class="container-list"></ul>');
 
@@ -17,5 +18,19 @@ describe('Editing the task description', () => {
     const list = localStorage.list[1];
     const taskList = list[0];
     expect(taskList.value[0].description).toBe('Play guitar');
+  })
+})
+
+describe('Updating an item completed status', () => {
+  test('update status', () => {
+    const list = localStorage.list[1];
+    const taskList = list[0];
+    const checkbox = dom.window.document.createElement('input')
+    checkbox.setAttribute('type', 'checkbox')
+    checkbox.setAttribute('data-id', 0)
+    checkbox.checked = true;
+    refrestCompletedTask(checkbox.checked, checkbox);
+    
+    expect(taskList.value[0].completed).toBe(true)
   })
 })
