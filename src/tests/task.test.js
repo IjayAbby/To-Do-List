c86f7b45ebe223124335eec48f328929445a4311
LOctavio <luis_ram1997@hotmail.com>
2021-07-30 12:41:43 -0500

    Add test for adding element to DOM

--- src/tests/task.test.js
+++ src/tests/task.test.js
@@ -5,11 +5,17 @@ const LocalStorage = require('./localStorage')
 const setData = require('./setdata')
 const removeCompletedItem = require('./remove')
 const removeselectedItem = require('./removeTask')
+const createItemTask = require('./addItem.js');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 const myTask = new Task("", false, 0);
 const tasks = [];
 const input = "new task";
 const toDoTasks = require("./todo");
+
+const dom = new JSDOM(`<!DOCTYPE html><ul class="container-list"></ul>`);
+const listContainer = dom.window.document.querySelector('.container-list');
 
 global.localStorage = new LocalStorage();
 
@@ -29,6 +35,22 @@ describe ('add task', () => {
         list = list[0];
         expect(list.value.length).toBe(3);
     });
+
+   test('f', () => {
+       toDoTasks.forEach((task) => {
+        listContainer.insertAdjacentHTML(
+            'beforeend',
+            `<li data-id="${task.index}" draggable="true" class="item">
+            <input class="checkbox" type="checkbox" data-id="${task.index}">
+            <label class="text-task" data-id="${task.index}" contenteditable=true>${task.description}</label><br>
+            <i class="fas fa-trash-alt remove" data-id="${task.index}"></i>
+            <i class="fas fa-ellipsis-v icon-item"></i>
+            </li>
+            `,
+        );
+       });
+        expect(listContainer.getElementsByTagName("li").length).toBe(3);
+   });
 });
 
 describe('remove task', () => {
@@ -49,3 +71,4 @@ describe('remove task', () => {
     });
 });
 
+module.exports = dom;
